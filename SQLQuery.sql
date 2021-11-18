c53d0b2188b127be2945602dc0622d69cac8e1e1
nwa <68991507+getnwa@users.noreply.github.com>
2021-11-18 20:56:55 +0300

    Add files via upload

---
+++ SQLQuery.sql
@@ -0,0 +1,13 @@
+-- Create a new database called 'DatabaseMaster'
+-- Connect to the 'master' database to run this snippet
+USE master
+GO
+-- Create the new database if it does not exist already
+IF NOT EXISTS (
+    SELECT [name]
+        FROM sys.databases
+        WHERE [name] = N'DatabaseMaster'
+)
+CREATE DATABASE DatabaseMaster
+GO
+
